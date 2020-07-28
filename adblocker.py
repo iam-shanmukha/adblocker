c76a338719b5c7a2b6b414d5d0bbe02f3fd8c0cf
iam-shanmukha <vishnushanmukha@gmail.com>
2020-07-28 17:01:58 +0530

    platform update

--- adblocker.py
+++ adblocker.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
-import os,sys
+import os,sys, platform
 hosts = ["https://adaway.org/hosts.txt",
 "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"]
 count = 1
-try:
-	open('/etc/hosts', 'w').close()
-	for i in hosts:
-		cmd = "sudo curl -s {} >> /etc/hosts".format(i)
-		#print(cmd)
-		os.system(cmd)
-		print("completed {}/{}".format(count,len(hosts)))
-		count = count+1
-	print("Successfully Applied")
-	sys.exit()
-except PermissionError:
-	print("Please run as root \nsudo python adblocker.py")
-	sys.exit()
+if platform.system() == 'Linux':
+	print("Starting Script on Linux")
+	try:
+		open('/etc/hosts', 'w').close()
+		for i in hosts:
+			cmd = "sudo curl -s {} >> /etc/hosts".format(i)
+			#print(cmd)
+			os.system(cmd)
+			print("completed {}/{}".format(count,len(hosts)))
+			count = count+1
+		print("Successfully Applied")
+		sys.exit()
+	except PermissionError:
+		print("Please run as root \nsudo python adblocker.py")
+		sys.exit()
