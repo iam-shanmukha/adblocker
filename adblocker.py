ff025734c8e65a085159623da5037107ca7454c4
unknown <vishnushanmukha@gmail.com>
2020-07-28 14:06:10 +0530

    added support for window

--- adblocker.py
+++ adblocker.py
@@ -18,3 +18,24 @@ if platform.system() == 'Linux':
 	except PermissionError:
 		print("Please run as root \nsudo python adblocker.py")
 		sys.exit()
+
+elif platform.system() == 'Windows':
+        print("starting Script on Windows")
+        try:
+                os.popen('copy c:\Windows\System32\Drivers\etc\hosts c:\Windows\System32\Drivers\etc\hosts_backup /y')
+                open('c:\Windows\System32\Drivers\etc\hosts','w').close()
+                print("Backup old file Success")
+                for i in hosts:
+                        cmd = "curl -s {} -o c:\Windows\System32\Drivers\etc\hosts".format(i)
+                        os.system(cmd)
+                        print("completed {}/{}".format(count,len(hosts)))
+                        count = count+1
+                print("Successfully Blocked AD's")
+                sys.exit()
+        except PermissionError:
+                print("Abort! Please run as Administrator")
+                sys.exit()
+                        
+else:
+        print("Sorry! Platform Not Supported")
+        
